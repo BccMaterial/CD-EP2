0c52ebfb43969fb8b12407583f273591f6a2fc74
thiagopls1 <thiagopls1@hotmail.com>
2025-10-24 16:40:53 -0300

    add `dotenv` implementation with `main.py` refactor

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 from neo4j import GraphDatabase
 import pandas as pd
-
-#Configuração Neo4j:
-uri = ""
-user = ""
-password = ""
+import env
 
 #Leitura dos csv:
 movies_df = pd.read_csv("/csv/movies_amostra.csv")
 ratings_df = pd.read_csv("/csv/ratings_amostra.csv")
 
 #Conexão com o Neo4j:
-driver = GraphDatabase.driver(uri, auth=(user, password))
+driver = GraphDatabase.driver(env.db_uri, auth=(env.db_user, env.db_password))
 
 #def insert_ratings(tx, userId, movieId, rating, timestamp):
 
