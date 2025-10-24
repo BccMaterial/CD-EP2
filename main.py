06ea76f08f7d904eec5fd569e3ff84a2a899cf67
viniciusegydio <egydiovinicius022@gmail.com>
2025-10-24 15:52:11 -0300

    add comments

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ ratings_df = pd.read_csv("/csv/ratings_amostra.csv")
 #Conexão com o Neo4j:
 driver = GraphDatabase.driver(uri, auth=(user, password))
 
+#Modelagem do grafo:
+#Vértice "Usuário" com o atributo "userId"
+#Vértice "Filme" com os atributos "movieid", "title" e "genres"
+#Relação de "Usuário" para "Filme" chamada "Avaliação" com os atributos "rating" e "timestamp"
+
 #def insert_ratings(tx, userId, movieId, rating, timestamp):
 
 
