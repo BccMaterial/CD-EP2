9b0e1cb240b901d61c5ec1f519d7c3e24e914e8b
viniciusegydio <egydiovinicius022@gmail.com>
2025-10-25 16:20:28 -0300

    completes insert_usuario(), insert_movie() and criar_relacao_avaliacao()

--- main.py
+++ main.py
@@ -16,19 +16,32 @@ ratings_df = pd.read_csv("/csv/ratings_amostra.csv")
 
 def insert_usuario(tx, userId): #Completar e verificar
   query = """
-  MERGE(u:Usuario {userId: $userId}
+  MERGE(u:Usuario {userId: $userId})
   """
+  tx.run(query, userId=userId)
 
-def insert_movies(tx, movieId, title, genres): #Completar e verificar
+def insert_movie(tx, movieId, title, genres): #Completar e verificar
   query = """
   MERGE(f:Filme {movieId: $movieId} ) 
   SET f.title = $title, f.genres = $genres
   """
+  tx.run(query, movieId=movieId, title=title, genres=genres)
 
-def criar_relacao_avaliacao(tx, user_id, movie_id, rating): #Completar e verificar 
+def criar_relacao_avaliacao(tx, userId, movieId, rating, timestamp): #Completar e verificar
   query = """
-    MATCH = (u:Usuario {user_id: $user_id})
-    MATCH = (f:Filme {movie_id: $movie_id})
+    MATCH = (u:Usuario {userId: $userId})
+    MATCH = (f:Filme {movieId: $movieId})
     Merge = (u) -[a:AVALIA]->(f)
-    SET a.rating = $rating 
+    SET a.rating = $rating, a.timestamp = $timestamp
     """
+  tx.run(query, userId=userId, movieId=movieId, rating=rating, timestamp=timestamp)
+
+  #Inserções:
+
+  with driver.session() as session: #driver.session() é o objeto utilizado para realizar queries no banco de dados
+    #Inserção dos filmes:
+    for i, row in movies_df.iterrows():
+      session.execute_write(insert_movie, row["movieId"], row["tile"], row["genres"])
+      
+    #Inserção de usuário e relações:
+
